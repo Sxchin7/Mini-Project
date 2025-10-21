625698197c6358edfef4a630d15878489ea4a57f
Sxchin7 <svkurup2003@gmail.com>
2025-10-21 21:00:27 +0530

    Fix

---
+++ src/components/CarCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CarCard() {
+  return (
+    <div>
+      <p>Car Name</p>
+    </div>
+  )
+}
+
+export default CarCard
---
+++ src/data/carDetails.js
@@ -0,0 +1,22 @@
+const carDetails=[
+    {
+        id:1,
+        name:"Toyota Camry",
+        type:"Sedan"
+    },
+    {
+        id:2,
+        name:"Honda CR-V",
+        type:"SUV"
+    },
+    {
+        id:3,   
+        name:"Ford Mustang",
+        type:"Coupe"
+    },
+    {
+        id:4,
+        name:"Chevrolet Suburban",
+        type:"SUV"
+    }
+]
